b7b21212e02a4d79cb50847fa91c32eae639f4ff
Daichi Tamaki <tamakiii@users.noreply.github.com>
2017-07-06 01:22:36 +0900

    Update brew-anyenv.rb

--- brew-anyenv.rb
+++ brew-anyenv.rb
@@ -2,10 +2,11 @@ class Anyenv < Formula
   desc "anyenv - all in one for **env" if respond_to? :desc
   homepage "https://github.com/riywo/anyenv"
 
-	stable do
-    head "https://github.com/riywo/anyenv.git"
-    version "5693de4"
-  end
+  head "https://github.com/riywo/anyenv.git"
+  version "5693de4"
+
+  stable do
+    inreplace "libexec/anyenv", %Q|ANYENV_ROOT="${HOME}/.anyenv"|, %Q|ANYENV_ROOT="#{prefix}"|
 
   test do
     shell_output("eval \"$(#{bin}/anyenv init -)\" && anyenv version")
