a3e07ca4199fb038b24a1d8d0a7c42406bb9611a
Daichi Tamaki <tamakiii@users.noreply.github.com>
2017-07-06 01:34:20 +0900

    Update anyenv.rb

--- anyenv.rb
+++ anyenv.rb
@@ -7,6 +7,11 @@ class Anyenv < Formula
     version "1.0.0"
     sha256 "a1caf384b9d401c0c672514302b8fda15aee50aecb6e147f985fdf371be0e30b"
   end
+  
+  def install
+    inreplace "libexec/anyenv", %Q|ANYENV_ROOT="${HOME}/.anyenv"|, %Q|ANYENV_ROOT="#{prefix}"|
+    prefix.install Dir["*"]
+  end
 
   test do
     # shell_output("eval \"$(#{bin}/anyenv init -)\" && anyenv version")
