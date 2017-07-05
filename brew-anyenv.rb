e174a4dffdee32e91109ee3ca4e5b73bdb5e7b37
Daichi Tamaki <tamakiii@users.noreply.github.com>
2017-07-06 01:17:18 +0900

    Update brew-anyenv.rb

--- brew-anyenv.rb
+++ brew-anyenv.rb
@@ -1,22 +1,10 @@
 class Anyenv < Formula
   desc "anyenv - all in one for **env" if respond_to? :desc
   homepage "https://github.com/riywo/anyenv"
-  # url "https://github.com/riywo/anyenv/archive/v1.0.0.tar.gz"
-  # shasum -a 256 master.tar.gz
-  # sha256 "b1c284f345027ecfa7d1e253c3e91bdfcab783696139763c79d446dd0969a0c3"
-  head "https://github.com/riywo/anyenv.git"
-  version "5693de4"
 
-  def install
-    inreplace "libexec/anyenv", %Q|ANYENV_ROOT="${HOME}/.anyenv"|, %Q|ANYENV_ROOT="#{prefix}"|
-    prefix.install Dir["*"]
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to the ~/.bashrc or ~/.zshrc file:
-
-      echo 'eval "$(anyenv init -)"' >> ~/.your_profile
-    EOS
+	stable do
+    head "https://github.com/riywo/anyenv.git"
+    version "5693de4"
   end
 
   test do
